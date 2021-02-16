ca5fdb74285e44e3df23c7a45416081d9fc08ac2
Miguel Gaspar <mdgaspar20@yahoo.com>
2021-02-16 18:22:20 -0500

    Adding files

---
+++ TourOfHeroes/ToHModels/Hero.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ToHModels
+{
+
+    public class Hero
+    {
+        private string heroName;
+
+        public string HeroName
+        {
+            get { return heroName; }
+            set
+            {
+                if (value.Equals(null))
+                { //TODO: throw exception 
+                }
+                heroName = value;
+
+            }
+        }
+        public int HP { get; set; }
+        public Element ElementType { get; set; }
+        public SuperPower SuperPower { get; set; }
+    }
+}
